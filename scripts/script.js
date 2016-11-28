82aae16d5898fb2566f73eca39ee2ec9fe06d8dd
AwesomeSoleil <startsevd@gmail.com>
2016-11-28 09:09:15 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,2 @@
 $(document).ready(function() {
-    'use strict';
-    paper.install(window);
-    paper.setup(document.getElementById('mainCanvas'));
-    var tool = new Tool();
-    tool.onMouseDown = function(event) {
-        var circle = Shape.Circle(event.point.x, event.point.y, 20);
-        circle.fillColor = 'magenta';
-    };    
-    paper.view.draw();
 });
